3d18430b4230702d6872fe12b7c15449e7959292
Roderit <roderitorocu@gmail.com>
2024-05-18 15:08:49 -0500

    Uso de botones

--- ejemplo-callback.js
+++ ejemplo-callback.js
@@ -1,3 +1,9 @@
+const miBoton = document.getElementById("miboton");
+miBoton.addEventListener('click',
+(evento)=>{
+    console.log(evento);
+    alert("click en el boton");
+});
 setTimeout(()=>{
-    console.log('Ejecucion de timeout')
+    console.log('Ejecucion de timeout');
 }, 3000);
