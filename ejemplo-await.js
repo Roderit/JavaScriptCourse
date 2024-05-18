cd883b09ca87150c891a1bd0072f28ed21b24881
Roderit <roderitorocu@gmail.com>
2024-05-18 15:52:34 -0500

    Ejercicio4 (await)

---
+++ ejemplo-await.js
@@ -0,0 +1,18 @@
+
+async function miAsyncFunction(){
+    const miPromesaA = await miFuncion();
+    console.log(miPromesaA);
+}
+
+function miFuncion(){
+    const miPromesa = new Promise((resolve, reject)=>{
+        resolve("la promesa fue resuelta");
+        //reject("la promesa fallo");
+        /*setTimeout(()=>{
+            //resolve("set time out finalizo");
+            reject("set time out fallo");
+        },2000);*/
+    });
+
+    miPromesa.then((respuesta)=>console.log(respuesta), (razon)=>console.log(razon));
+}
